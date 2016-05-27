ae33a3e70ba3eaeea17644d11b84c9dde3b2eea9
nipil <nipil@users.noreply.github.com>
2016-05-27 17:25:48 +0200

    add basic slim skeleton

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php
-echo "it works";
-?>
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\App();
+
+$app->group('/jcdecaux_history_api', function () use ($app) {
+
+    $app->get('', function ($request, $response, $args) {
+        return "jcdecaux_history_api is working";
+    });
+
+});
+
+$app->run();
